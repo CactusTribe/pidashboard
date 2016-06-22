330985d87f73c2030cddc893248cc47b8b980795
CactusTribe <lefrancjoaquim@gmail.com>
2016-06-22 09:39:06 +0200

    Ajout : xplanet, heure, météo

---
+++ inc.php
@@ -0,0 +1,8 @@
+<?php 
+
+function meteo () {
+  $meteo = '<div id="cont_NzUwNTZ8NHwzfDF8NHwwMDAwMDB8MXxGRkZGRkZ8Y3wx"><div id="spa_NzUwNTZ8NHwzfDF8NHwwMDAwMDB8MXxGRkZGRkZ8Y3wx"><a id="a_NzUwNTZ8NHwzfDF8NHwwMDAwMDB8MXxGRkZGRkZ8Y3wx" href="http://www.meteocity.com/france/paris_v75056/" target="_blank" style="color:#333;text-decoration:none;">Météo Paris</a></div><script type="text/javascript" src="http://widget.meteocity.com/js/NzUwNTZ8NHwzfDF8NHwwMDAwMDB8MXxGRkZGRkZ8Y3wx"></script></div>';
+  return $meteo;
+}
+
+?>
